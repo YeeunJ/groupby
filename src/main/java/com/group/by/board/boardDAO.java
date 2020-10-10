55d7a5810255fe491101fa7d205e02eadfa000cb
YeeunJ <21800669@handong.edu>
2020-10-11 03:53:53 +0900

    dashboard allboard data

--- src/main/java/com/group/by/board/boardDAO.java
+++ src/main/java/com/group/by/board/boardDAO.java
@@ -1,5 +1,47 @@
 package com.group.by.board;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.group.by.dbconnection.connection;
+import com.group.by.dto.progressDTO;
+
 public class boardDAO {
+	private Connection conn = connection.getInstance().getConnection();
+	protected PreparedStatement pstmt1, pstmt2;
+	protected ResultSet rs1, rs2;
+	private StringBuffer query1, query2;
+	protected String sql1, sql2;
+	protected Boolean result = false;
+	
+	public ArrayList<progressDTO> getmyBoardInfo(int userID) throws ClassNotFoundException, SQLException {
+		ArrayList<progressDTO> groupData = new ArrayList<progressDTO>();
+		sql1 = "select info.group_info as id, gi.name as name, count(*) as allMission from mission_info as info right join group_info as gi on gi.id = info.group_info where group_info in (select g.id from group_info as g join participation as p on p.groupID = g.id where p.userID = 1) group by group_info order by group_info;";
+		sql2 = "select info.group_info as id, gi.name as name, count(*) as completeMission from mission_info as info right join group_info as gi on gi.id = info.group_info where group_info in (select g.id from group_info as g join participation as p on p.groupID = g.id where p.userID = ?) and info.complete = 1 group by group_info order by group_info;";
+	    query1 = new StringBuffer();
+	    query2 = new StringBuffer();
+	    query1.append(sql1);
+	    query2.append(sql2);
+	    pstmt1 = conn.prepareStatement(query1.toString());
+	    pstmt2 = conn.prepareStatement(query2.toString());
+	    pstmt1.setInt(1, userID);
+	    pstmt2.setInt(1, userID);
+	    rs1 = pstmt1.executeQuery();
+	    rs2 = pstmt2.executeQuery();
+	    while(rs1.next()) {
+	    	rs2.next();
+	    	progressDTO data = new progressDTO();
+	    	data.setId(rs1.getInt("allMission"));;
+	    	data.setName(rs1.getString("name"));
+	    	data.setAllMission(rs1.getInt("allMission"));
+	    	data.setCompleteMission(rs2.getInt("completeMission"));
+	    	groupData.add(data);
+	    }
+		return groupData;
+	}
+	
 
 }
