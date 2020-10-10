2469b5aaf86dbcea41207b60cac00758efd70a21
YeeunJ <21800669@handong.edu>
2020-10-11 01:13:36 +0900

    userdao

--- src/main/java/com/group/by/users/usersDAO.java
+++ src/main/java/com/group/by/users/usersDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.group.by.dbconnection.connection;
+import com.group.by.dto.usersDTO;
 
 public class usersDAO {
 	private Connection conn = connection.getInstance().getConnection();
@@ -15,11 +16,44 @@ public class usersDAO {
 	protected String sql;
 	protected Boolean result = false;
 	
-	public final void prepareDB() throws ClassNotFoundException, SQLException {
-		
+	public usersDTO getUserInfo(String email) throws ClassNotFoundException, SQLException {
+		usersDTO userData = new usersDTO();
+		sql = "select * from users where email = ?";
 	    query = new StringBuffer();
 	    query.append(sql);
-	    
 	    pstmt = conn.prepareStatement(query.toString());
+	    pstmt.setString(1, email);
+	    rs = pstmt.executeQuery();
+	    if(rs.next()) {
+	    	userData.setId(rs.getInt("id"));;
+	    	userData.setEmail(rs.getString("email"));
+	    	userData.setName("name");
+	    	userData.setAge(rs.getInt("age"));;
+	    	userData.setIntroduce(rs.getString("introduce"));
+	    }
+	    updateLogin(userData.getId());
+		return userData;
+	    
+	}
+	
+	public void updateLogin(int id) throws ClassNotFoundException, SQLException {
+		sql = "UPDATE users SET lastLoginDate = now(), loginCount=loginCount+1 WHERE id = ?;";
+		query = new StringBuffer();
+	    query.append(sql);
+	    pstmt = conn.prepareStatement(query.toString());
+	    pstmt.setInt(1, id);
+	    pstmt.executeUpdate();
+	}
+	public void getUserInfo(usersDTO updateData) throws ClassNotFoundException, SQLException {
+		sql = "UPDATE users SET email = ?, name = ?, age = ?, introduce = ?, modifyDate = now() WHERE id = ?;";
+	    query = new StringBuffer();
+	    query.append(sql);
+	    pstmt = conn.prepareStatement(query.toString());
+	    pstmt.setString(1, updateData.getEmail());
+	    pstmt.setString(2, updateData.getName());
+	    pstmt.setInt(3, updateData.getAge());
+	    pstmt.setString(4, updateData.getIntroduce());
+	    pstmt.setInt(5, updateData.getId());
+	    pstmt.executeUpdate();
 	}
 }
