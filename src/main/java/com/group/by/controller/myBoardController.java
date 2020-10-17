06d18d354d72b18e942702fe1b9fd8595d678160
YeeunJ <21800669@handong.edu>
2020-10-17 21:30:20 +0900

    controller update

--- src/main/java/com/group/by/controller/myBoardController.java
+++ src/main/java/com/group/by/controller/myBoardController.java
@@ -13,10 +13,18 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class myBoardController {
 	//myboard는 나중에 뒤에 링크 넣어서 할거야!! 지금은 일단 이렇게!!
-	@RequestMapping("/myBoard")
+	@RequestMapping("/myBoard, method = RequestMethod.GET")
 	public ModelAndView my() {        
 		ModelAndView model = new ModelAndView("myBoard");
-		model.addObject("여기에다가", "정보담아서 전달 가능해!!");
+		//model.addObject("여기에다가", "정보담아서 전달 가능해!!");
+		return model;
+	}
+	
+	@RequestMapping("/myBoard, method = RequestMethod.POST")
+	public ModelAndView myupload() {        
+		ModelAndView model = new ModelAndView("myBoard");
+		
+		//model.addObject("여기에다가", "정보담아서 전달 가능해!!");
 		return model;
 	}
 	
